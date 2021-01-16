8ee2718a97a07c7fdd857ce76c101ea3600a5019
kulsoomzahra <kulsoomzahra24@gmail.com>
2021-01-17 00:02:15 +0530

    Update forms.py

--- poll/forms.py
+++ poll/forms.py
@@ -1,6 +1,9 @@
 from django.forms import ModelForm
 from .models import Poll
 
+
+#A model form will simply take an existing model in your app and create a form based off it.
+
 class CreatePollForm(ModelForm):
     class Meta:
         model = Poll
