5842a55bfaddc54019c719a659c2e5b971d13c06
kulsoomzahra <kulsoomzahra24@gmail.com>
2022-06-11 23:22:50 +0530

    host

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = '#okmj(sg2y1@vqz1p^!y=-!j(=3j!=5%wyir=2icww_06wny44'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'thepollingbooth.herokuapp.com'
-    u'0.0.0.0'
-    
+    'https://thepollingbooth.herokuapp.com', 'http://thepollingbooth.herokuapp.com',
+    '0.0.0.0', '127.0.0.1','thepollingbooth.herokuapp.com'
 ]
 
 
